84be10b2d272aaa74dcde4adbcb0cb4cabe5084c
WaProject - Vilhenna Jr <flavianovilhena@gmail.com>
2022-04-06 22:59:57 -0300

    feat: User entity

---
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,33 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { IUser } from '@modules/users/domain/models/IUser';
+
+@Entity('users')
+class User implements IUser {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
