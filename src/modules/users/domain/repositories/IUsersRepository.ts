8ce9fdef420c9604161f3ddecbaaf4e4ebf4a108
WaProject - Vilhenna Jr <flavianovilhena@gmail.com>
2022-04-07 14:22:13 -0300

    feat: Repository and User Service

--- src/modules/users/domain/repositories/IUsersRepository.ts
+++ src/modules/users/domain/repositories/IUsersRepository.ts
@@ -2,5 +2,6 @@ import { ICreateUser } from '../models/ICreateUser';
 import { IUser } from '../models/IUser';
 
 export interface IUsersRepository {
+  findByEmail(email: string): Promise<IUser | undefined>;
   create(data: ICreateUser): Promise<IUser>;
 }
---
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,32 @@
+import { ICreateUser } from '@modules/users/domain/models/ICreateUser';
+import { IUsersRepository } from '@modules/users/domain/repositories/IUsersRepository';
+import { getRepository, Repository } from 'typeorm';
+import User from '../entities/User';
+
+class UsersRepository implements IUsersRepository {
+  private ormRepository: Repository<User>;
+
+  constructor() {
+    this.ormRepository = getRepository(User);
+  }
+
+  public async create({ name, email, password }: ICreateUser): Promise<User> {
+    const user = this.ormRepository.create({ name, email, password });
+
+    await this.ormRepository.save(user);
+
+    return user;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.ormRepository.findOne({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  }
+}
+
+export default UsersRepository;
